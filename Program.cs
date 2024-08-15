6c0496c4dd5c0adfd674600876c1d6ac218bd9d3
Douglas Vicente <douglas.vinicius001@gmail.com>
2024-08-15 15:04:45 -0300

    criando repositório e primeira aula

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+//variáveis ou propridedaes, podem ser modificadas a todo momento do ciclo de vida do código.
+int idade;
+idade = 25;
+System.Console.WriteLine(idade);
+
+//Não se pode alterar os valores de constantes ao decorrer do projeto (digamos que você crie uma constante com um valor x, ele nunca poderá ser substituido ou alterado).
+const string msgParabens = "parabéns pela sua idade";
+System.Console.WriteLine(msgParabens);
+//valor2 = valor;
+
+string nome = "Professor Douglas";
+System.Console.WriteLine(nome + " - idade " + idade + msgParabens);
+Console.ReadKey();
